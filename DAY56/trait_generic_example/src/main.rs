570d548259eb6d84d58dd6eadd3cf577f011813b
BhaT844 <bluehart_777@naver.com>
2023-08-19 01:27:09 +0900

    DAY56 commit :)

---
+++ DAY56/trait_generic_example/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::Write;
+
+fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
+    out.write_all(b"hello world\n")?;
+    out.flush()
+}
+
+fn min<T: Ord>(value1: T, value2: T) -> T {
+    if value1 <= value2 {
+        value1
+    } else {
+        value2
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    use std::fs::File;
+    let mut local_file = File::create("hello.txt")?;
+    say_hello(&mut local_file)?;
+
+    let mut bytes = vec![];
+    say_hello(&mut bytes)?;
+    assert_eq!(bytes, b"hello world\n");
+
+    Ok(())
+}
