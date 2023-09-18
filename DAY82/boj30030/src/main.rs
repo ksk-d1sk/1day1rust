1b18c7a6633e06ea2d3d6dff90d1cec6789b2a0d
BhaT844 <bluehart_777@naver.com>
2023-09-19 00:58:50 +0900

    DAY82 commit :)

---
+++ DAY82/boj30030/src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+
+    macro_rules! get {
+        ( $($t:ty),+ ) => { ($(input.next().unwrap().parse::<$t>().unwrap()),+) };
+    }
+
+    print!("{}", get!(u32) * 10 / 11);
+}
