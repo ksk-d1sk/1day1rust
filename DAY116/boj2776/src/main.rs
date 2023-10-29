a58462dbde96a625246395fdb60fcb64e4e55cd8
BhaT844 <bluehart_777@naver.com>
2023-10-30 00:31:50 +0900

    DAY116 commit :)

---
+++ DAY116/boj2776/src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input  = buf.split_ascii_whitespace();
+    let mut output = String::new();
+
+    macro_rules! get {
+        () => { input.next().unwrap() };
+        ( $($t:ty),+ ) => { ($(input.next().unwrap().parse::<$t>().unwrap()),+) };
+    }
+
+    for _ in 0..get!(usize) {
+        let set: HashSet<_> = (0..get!(u32)).map(|_| get!()).collect();
+        for _ in 0..get!(u32) {
+            let _ = writeln!(output, "{}", u8::from(set.contains(get!())));
+        }
+    }
+
+    print!("{output}");
+}
