d5caf63550d1c5837149182ac36de9e2546361b5
BhaT844 <bluehart_777@naver.com>
2023-11-03 00:55:59 +0900

    DAY120 commit :)

---
+++ DAY120/fuse_example/src/main.rs
@@ -0,0 +1,26 @@
+struct Flaky(bool);
+
+impl Iterator for Flaky {
+    type Item = &'static str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 {
+            self.0 = false;
+            Some("totally the last item")
+        } else {
+            self.0 = true;
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut flaky = Flaky(true);
+    assert_eq!(flaky.next(), Some("totally the last item"));
+    assert_eq!(flaky.next(), None);
+    assert_eq!(flaky.next(), Some("totally the last item"));
+
+    let mut not_flaky = Flaky(true).fuse();  // None을 반환한 시점부터 계속 None을 반환해준다.
+    assert_eq!(not_flaky.next(), Some("totally the last item"));
+    assert_eq!(not_flaky.next(), None);
+    assert_eq!(not_flaky.next(), None);
+}
