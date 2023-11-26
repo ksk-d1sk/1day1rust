cad38a4f6785d8db956a6c1e4681be1e8bcd96bf
BhaT844 <bluehart_777@naver.com>
2023-11-27 00:37:23 +0900

    DAY140 commit :)

---
+++ DAY140/vec_to_string/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let good_utf8: Vec<u8> = vec![0xe9, 0x8c, 0x86];
+    assert_eq!(String::from_utf8(good_utf8).ok(), Some("錆".to_string()));
+
+    let bad_utf8: Vec<u8> = vec![0x9f, 0xf0, 0xa6, 0x80];
+    let result = String::from_utf8(bad_utf8);
+    assert!(result.is_err());
+    assert_eq!(result.unwrap_err().into_bytes()
+             , [0x9f, 0xf0, 0xa6, 0x80]);
+}
