d07df51282016b82441d53fdb4247c213cf18180
BhaT844 <bluehart_777@naver.com>
2024-02-05 00:33:17 +0900

    DAY195 commit :)

---
+++ DAY195/boj14065/src/main.rs
@@ -0,0 +1,14 @@
+use std::io::*;
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let mut tokens = input.split_ascii_whitespace();
+
+    macro_rules! next {
+        ( $($t:ty),+ ) => { ($(tokens.next().unwrap().parse::<$t>().unwrap()),+) };
+    }
+
+    let x = next!(f64);
+
+    println!("{}", 378541178.4 / (1609344.0 * x));
+}
