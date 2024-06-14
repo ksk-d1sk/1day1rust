689b877b20cb2fa7aa15a7906c80928d5806ebb5
ksk_d1sk <bluehart_777@naver.com>
2024-06-15 02:56:10 +0900

    DAY303 commit :)

---
+++ DAY303/boh16488/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::*;
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let mut tokens = input.split_ascii_whitespace();
+
+    macro_rules! next {
+        () => { tokens.next().unwrap() };
+        ( $($t:ty),+ ) => { ($(tokens.next().unwrap().parse::<$t>().unwrap()),+) };
+    }
+
+    let (n, k) = next!(u64, u64);
+    
+    print!("{}", n * n * k);
+}
